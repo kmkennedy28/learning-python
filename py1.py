a7c1d41d418778633f18c4c552724140dc59b3c5
Keira Kennedy <kmkennedy2006@gmail.com>
2025-07-27 21:07:12 -0400

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,18 @@
+print('Hello World!')
+
+#ask for user's name
+print('What is your name?')
+myName = input('>');
+#save and print name
+print('It is good to meet you, ' + myName)
+
+#get length of name
+print('The length of your name is: ')
+print(len(myName))
+
+#calculations
+#ask for and store their age
+print('What is your age?')
+myAge = input('>')
+#take age as an integer, add one, and print it as a string
+print('You will be ' + str(int(myAge) + 1 ) + ' in a year.')
