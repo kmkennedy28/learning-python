39c79cac08c2c99fc0b8618d43d63ce083be5465
Keira Kennedy <kmkennedy2006@gmail.com>
2025-08-05 21:59:04 -0400

    Add files via upload

--- py5.py
+++ py5.py
@@ -1,41 +1,44 @@
-#Say you have a list value like this:
+import logging
+import random
+#normal configuration
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s -  %(levelname)s -  %(message)s')
 
-#spam = ['apples', 'bananas', 'tofu', 'cats']
-#Write a function that takes a list value as an argument and returns a string with all the items separated by a comma and a space, with and inserted 
-#before the last item. For example, passing the previous spam list to the function would return 'apples, bananas, tofu, and cats'. But your function 
-#should be able to work with any list value passed to it. Be sure to test the case where an empty list [] is passed to your function.
+#ignore the logged debug messages
+logging.disable(logging.CRITICAL)
 
-#takes a list and puts all the values into a string with ', ' 
-def toPrint(spam):
-    length = len(spam)
-    if length == 0:
-        return 'List cannot be empty'
-    elif length == 1:
-        return spam[0]
+logging.debug('Start of program')
 
-    elif length == 2:
-        return spam[0] + ' and ' + spam[1]
+#holds the user's coin toss guess
+guess = ''
 
+#get users guess, must be heads or tails
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input()
+
+#randomize an int to be heads or tails
+toss = random.randint(0, 1)
+
+#assign 0 as tails, 1 as heads
+if toss == 0:
+    result = 'tails'
+elif toss == 1:
+    result = 'heads'
+
+logging.debug('Results = ' + result + ' guess = ' + guess)
+
+#compare the result to the user's guess
+if result == guess:
+    logging.debug('Results = guess')
+    print('You got it!')
+else:
+    #if first guess is wrong let user guess again
+    logging.debug('Results != guess')
+    print('Nope! Guess again!')
+    guess = input()
+    if result == guess:
+        print('You got it!')
     else:
-        result = ''
-        for i in range(length-1):
-            result += spam[i]
-            result += ', '
-        result += 'and '
-        result += spam[-1]
-        return result
+        print('Nope. You are really bad at this game.')
 
-
-spam = ['apples', 'bananas', 'tofu', 'cats']
-print(toPrint(spam))
-
-groceries = ['apples', 'grapes', 'pesto', 'pasta', 'butter', 'peanut butter']
-print(toPrint(groceries))
-
-supplies = ['pencils', 'scissors']
-print(toPrint(supplies))
-
-empty = []
-print(toPrint(empty))
-
-#needs error exception
+logging.debug('End of program')
